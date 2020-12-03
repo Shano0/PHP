01a3f821733cf8768440dc3912ddfb1de2794e1c
Shano0 <davit.shanidze.1@btu.edu.ge>
2020-12-03 20:45:12 +0400

    for giga

---
+++ database/seeds/ClassesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClassesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Classes::class, 2)->create();
+    }
+}
